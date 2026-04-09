765b0a40a10b544cff3cad844406a591663bf703
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2026-04-09 23:06:40 +0530

    fixed api

---
+++ quote-app/script.js
@@ -0,0 +1,35 @@
+async function getQuote() {
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+
+quoteEl.innerText = "Loading...";
+authorEl.innerText = "";
+
+try {
+const res = await fetch("https://api.quotable.io/random");
+const data = await res.json();
+
+typeEffect(`"${data.content}"`, quoteEl);
+authorEl.innerText = `- ${data.author}`;
+
+} catch (err) {
+quoteEl.innerText = "Failed to load quote 😢";
+}
+}
+
+function typeEffect(text, element) {
+element.innerText = "";
+let i = 0;
+
+const interval = setInterval(() => {
+element.innerText += text[i];
+i++;
+if (i >= text.length) clearInterval(interval);
+}, 20);
+}
+
+function copyQuote() {
+const text = document.getElementById("quote").innerText;
+navigator.clipboard.writeText(text);
+alert("Copied!");
+}
